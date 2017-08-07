48761b1dc6ffa9e52a18ec6962d903d84ec1845b
Harshit Pant <pantharshit00@gmail.com>
2017-08-07 08:53:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,5 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
 };
 
 $('.center').on('click',function(){
-    this.firstElementChildren.src = 'https://ichef.bbci.co.uk/news/660/cpsprodpb/025B/production/_85730600_monkey2.jpg';
+    this.firstElementChild.src = 'https://ichef.bbci.co.uk/news/660/cpsprodpb/025B/production/_85730600_monkey2.jpg';
 });
